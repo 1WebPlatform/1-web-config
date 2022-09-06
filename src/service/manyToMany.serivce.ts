a9f50a006426b45a7bc6e2f7ff81ce46df4e6314
Semenov <semenov@mail.ru>
2022-09-06 22:21:55 +0400

    TB-5 many_to_many

--- src/service/manyToMany.serivce.ts
+++ src/service/manyToMany.serivce.ts
@@ -2,7 +2,7 @@ export class ManyToManySerivce {
     private readonly name_table: string;
     private readonly name_schema: string;
     private readonly name_column: string[]; // 2 значения 
-    private readonly errors_404: string[]; // 2  значения
+    private readonly errors_404: string[]; // 3  значения
     private sql: string = "";
 
     constructor(
@@ -27,8 +27,8 @@ export class ManyToManySerivce {
         this.sql += `
 CREATE TABLE ${this.name_schema}.${this.name_table} (
     id int4 NOT NULL GENERATED ALWAYS AS IDENTITY,
-    id_right int4 NOT NULL,
-    id_roles int4 NOT NULL,
+    id_${this.name_column[0]} int4 NOT NULL,
+    id_${this.name_column[1]} int4 NOT NULL,
     CONSTRAINT ${this.name_table}_pk PRIMARY KEY (id),
     CONSTRAINT ${this.name_table}_fk FOREIGN KEY (id_${this.name_column[0]}) REFERENCES ${this.name_schema}.${this.name_column[0]}(id),
     CONSTRAINT ${this.name_table}_fk_1 FOREIGN KEY (id_${this.name_column[1]}) REFERENCES ${this.name_schema}.${this.name_column[1]}(id)
@@ -36,21 +36,36 @@ CREATE TABLE ${this.name_schema}.${this.name_table} (
     }
     private generatorSave(){
         this.sql +=`
-        CREATE OR REPLACE FUNCTION ${this.name_schema}.${this.name_table}_save(
-            _id_${this.name_column[0]} int,
-            _id_${this.name_column[1]} int,
-            OUT id_ int,
-            OUT error tec.error
-        )
-        LANGUAGE plpgsql
-        AS $function$
-            BEGIN
-            INSERT INTO ${this.name_schema}.${this.name_table} 
-                (id_${this.name_column[0]},id_${this.name_column[1]}) 
-                VALUES (_id_${this.name_column[0]},  _id_${this.name_column[1]}) 
-                RETURNING id INTO id_;		
-            END;
-        $function$;
+CREATE OR REPLACE FUNCTION ${this.name_schema}.${this.name_table}_save(
+    _id_${this.name_column[0]} int,
+    _id_${this.name_column[1]} int,
+    out id_ int,
+    out error tec.error
+)
+LANGUAGE plpgsql
+AS $function$
+
+declare
+    check_${this.name_column[0]} boolean;
+    check_${this.name_column[1]} boolean;
+    ids int[];
+    BEGIN   
+        check_${this.name_column[0]} := (select * from tec.${this.name_column[0]}_check_id(_id_${this.name_column[0]}));
+        check_${this.name_column[1]} := (select * from tec.${this.name_column[1]}_check_id(_id_${this.name_column[1]}));
+        if check_${this.name_column[0]} <> true and check_${this.name_column[1]} <> true then
+            select * into error  from tec.error_get_id(${this.errors_404[0]});
+        elseif check_${this.name_column[0]} <> true then 
+            select * into error  from tec.error_get_id(${this.errors_404[1]});
+        elseif check_${this.name_column[1]} <> true then 
+            select * into error  from tec.error_get_id(${this.errors_404[2]});
+        else 
+            INSERT INTO  ${this.name_schema}.${this.name_table}
+            (id_${this.name_column[0]},id_${this.name_column[1]}) 
+            VALUES (_id_${this.name_column[0]},  _id_${this.name_column[1]}) 
+            RETURNING id INTO id_;
+        end if;
+    end;
+$function$;
 `;
     }
 }
