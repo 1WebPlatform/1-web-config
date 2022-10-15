6af40ad81c8cb1f7c0084bb946a1f6ef2c7d4c6b
Semenov <semenov@mail.ru>
2022-10-15 10:01:43 +0400

    TB-7 из css file body css config

--- src/service/css.service.ts
+++ src/service/css.service.ts
@@ -1,13 +1,13 @@
 export class CssService{
     convertCssToJson(css:string){
         const res:any = [];
-        const selectBody = /[{.a-z: \n;\d\\\\*\/#-]+}/gi;
-        const cssBody = /{[a-z: \n;\d\\\\*\/#-]+}/gi;
+        const selectBody = /[{.a-z:% \n;\d\\\\*\/#-]+}/gi;
+        const cssBody = /{[a-z:% \n;\d\\\\*\/#-]+}/gi;
         const selectName = /.+{/gi;
 
-        const selectArray = css.match(selectBody);
+        const selectArray = css.match(selectBody);        
         if (selectArray.length) {
-            selectArray.map((s)=>{
+            selectArray.map((s)=>{               
                 const body = s.match(cssBody)[0].slice(1,-1).split(";");
                 const bodyRes:any = {};
                 for (const elem of body) {
